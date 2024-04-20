bf6d40e9474f634db5497919dfd0cdb88b9048ea
gaojk <13306758+xiaogaochao@user.noreply.gitee.com>
2024-04-20 12:35:31 +0800

    my_print correct

--- source/python/lesson4/my-print.py
+++ source/python/lesson4/my-print.py
@@ -5,20 +5,23 @@ import io
 output = io.StringIO()
 sys.stdout = output
 
-def my_print(*args, sep=' ', end='\n', file=sys.stdout, flush=False):
-    print(*args, sep=sep, end=end, file=file, flush=flush)
+def my_print(*args, sep=' ', end='\n'):
+    file = open("my_print_output.txt", "a")
+    content = sep.join(map(str, args)) + end
+    file.write(content)
+    file.close()
 
-
-
+file = open("my_print_output.txt", "w")
+file.close()
 a = 1
-with open('my_print_output.txt', 'w') as f1, open('print_output.txt', 'w') as f2:
+my_print('Hello, World')
+my_print('1111111', end = " ")
+my_print(f"{a}") 
+my_print("222", a)
+
+with open('print_output.txt', 'w') as f1:
     sys.stdout = f1
-    my_print('Hello', 'World')
-    my_print('1111111', end = " ")
-    my_print(f"{a}")
-    my_print("222", a)
-    sys.stdout = f2
-    print('Hello', 'World')
+    print('Hello, World')
     print('1111111', end = " ")
     print(f"{a}")
     print("222", a)
